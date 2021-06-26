ee4dff1c351651355076305cc8a72b0760b09c2e
Isabela Lantero <lantero.isabela@gmail.com>
2021-06-26 10:27:51 +0200

    "esta todo"

--- src/Lesson1.R
+++ src/Lesson1.R
@@ -6,4 +6,32 @@
 a <- 5
 b <- 3
 
+# ejecutar: cmd + enter
 
+
+#seq
+nmrs <- seq(40)
+nmrs
+1:40
+seq(from=4,40,2)
+
+# c
+# vectors~tupla en python
+primer_vector <- c(5, 1, 4, 4, 3, 3, 8, 9)
+primer_vector
+segundo_vector <- c(7, 4, 3, 8, 9, 1, 1, 6)
+segundo_vector
+primer_vector + segundo_vector
+# todas las operaciones/funciones están vectorizadas!!!
+
+primer_vector + 6
+primer_vector + c(4, 6)
+
+tercer_v <- c(4, 5, 6, 7, 8)
+tercer_v + c(9, 2, 2)
+# RECICLAJEEE!!!
+
+vtor_ketras <- letters[1:6]
+paste(vtor_ketras, LETTERS[1:6])
+paste(vtor_ketras, "A")
+paste(vtor_ketras, LETTERS[1:3], sep = " - ")
